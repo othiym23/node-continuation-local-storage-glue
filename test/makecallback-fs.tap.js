5154bef41e86ac241faf78132ad623819c509eea
Forrest L Norvell <forrest@newrelic.com>
2013-07-23 22:40:17 -0700

    More fs tests.
    Fixed test cases to deal with the absence of graceful-fs, which assumes
    that the latest version of glob is getting installed.

--- test/makecallback-fs.tap.js
+++ test/makecallback-fs.tap.js
@@ -9,20 +9,41 @@ var fs              = require('fs')
   , createNamespace = require('continuation-local-storage').createNamespace
   ;
 
-var FILENAME = '__testfile';
+// CONSTANTS
+var FILENAME = '__testfile'
+  , LINKNAME = '__testlink'
+  ;
 
 function createFile(assert) {
   var contents = new Buffer("UHOH")
-    , file     = fs.openSync(FILENAME, 'w', '0777')
+    , file     = fs.openSync(FILENAME, 'w')
     , written  = fs.writeSync(file, contents, 0, contents.length, 0)
     ;
 
   assert.equals(written, contents.length, "whole buffer was written");
 
-  return fs.closeSync(file);
+  var rc = fs.closeSync(file);
+  // need this here to avoid dealing with umask complications
+  fs.chmod(FILENAME, '0666');
+  return rc;
 }
 
 function deleteFile() { return fs.unlinkSync(FILENAME); }
+
+
+function createLink(assert) {
+  createFile(assert);
+
+  fs.symlinkSync(FILENAME, LINKNAME);
+  fs.lchmodSync(LINKNAME, '0777');
+}
+
+function deleteLink() {
+  fs.unlinkSync(LINKNAME);
+
+  return deleteFile();
+}
+
 
 function mapIds(username, groupname, callback) {
   if (!callback) throw new Error("mapIds requires callback");
@@ -45,7 +66,7 @@ function mapIds(username, groupname, callback) {
 }
 
 test("continuation-local state with MakeCallback and fs module", function (t) {
-  t.plan(5);
+  t.plan(12);
 
   var namespace = createNamespace('fs');
   namespace.set('test', 0xabad1dea);
@@ -130,10 +151,8 @@ test("continuation-local state with MakeCallback and fs module", function (t) {
         namespace.set('test', 'chown');
         t.equal(namespace.get('test'), 'chown', "state has been mutated");
 
-        // this will fail silently as long as node-tap is using graceful-fs,
-        // because it swallows EPERMs returned by chown
         fs.chown(FILENAME, uid, gid, function (error) {
-          t.notOk(error, "changing ownership shouldn't error");
+          t.ok(error, "changing ownership will error for non-root users");
 
           t.equal(namespace.get('test'), 'chown',
                   "mutated state has persisted to fs.chown's callback");
@@ -159,10 +178,8 @@ test("continuation-local state with MakeCallback and fs module", function (t) {
         t.equal(namespace.get('test'), 'fchown', "state has been mutated");
 
         var file = fs.openSync(FILENAME, 'w');
-        // this will fail silently as long as node-tap is using graceful-fs,
-        // because it swallows EPERMs returned by chown
         fs.fchown(file, uid, gid, function (error) {
-          t.notOk(error, "changing ownership shouldn't error");
+          t.ok(error, "changing ownership will error for non-root users");
 
           t.equal(namespace.get('test'), 'fchown',
                   "mutated state has persisted to fs.fchown's callback");
@@ -175,18 +192,176 @@ test("continuation-local state with MakeCallback and fs module", function (t) {
     });
   });
 
-  // TODO: 'lchown'
-  // TODO: 'chmod'
-  // TODO: 'fchmod'
-  // TODO: 'lchmod'
-  // TODO: 'stat'
-  // TODO: 'lstat'
-  // TODO: 'fstat'
+  t.test("fs.lchown", function (t) {
+    createLink(t);
+
+    mapIds('daemon', 'daemon', function (error, uid, gid) {
+      t.notOk(error, "looking up uid & gid shouldn't error");
+      t.ok(uid, "uid for daemon was found");
+      t.ok(gid, "gid for daemon was found");
+
+      var context = namespace.createContext();
+      context.run(function () {
+        namespace.set('test', 'lchown');
+        t.equal(namespace.get('test'), 'lchown', "state has been mutated");
+
+        fs.lchown(LINKNAME, uid, gid, function (error) {
+          t.ok(error, "changing ownership will error for non-root users");
+
+          t.equal(namespace.get('test'), 'lchown',
+                  "mutated state has persisted to fs.lchown's callback");
+
+          deleteLink();
+          t.end();
+        });
+      });
+    });
+  });
+
+  t.test("fs.chmod", function (t) {
+    createFile(t);
+
+    var context = namespace.createContext();
+    context.run(function () {
+      namespace.set('test', 'chmod');
+      t.equal(namespace.get('test'), 'chmod', "state has been mutated");
+
+      fs.chmod(FILENAME, '0700', function (error) {
+        t.notOk(error, "changing mode shouldn't error");
+
+        t.equal(namespace.get('test'), 'chmod',
+                "mutated state has persisted to fs.chmod's callback");
+
+        var stats = fs.statSync(FILENAME);
+        t.equal(stats.mode.toString(8), '100700', "extra access bits are stripped");
+
+        deleteFile();
+        t.end();
+      });
+    });
+  });
+
+  t.test("fs.fchmod", function (t) {
+    createFile(t);
+
+    var context = namespace.createContext();
+    context.run(function () {
+      namespace.set('test', 'fchmod');
+      t.equal(namespace.get('test'), 'fchmod', "state has been mutated");
+
+      var file = fs.openSync(FILENAME, 'w+');
+      fs.fchmod(file, '0700', function (error) {
+        t.notOk(error, "changing mode shouldn't error");
+
+        t.equal(namespace.get('test'), 'fchmod',
+                "mutated state has persisted to fs.fchmod's callback");
+
+        fs.closeSync(file);
+        var stats = fs.statSync(FILENAME);
+        t.equal(stats.mode.toString(8), '100700', "extra access bits are stripped");
+
+        deleteFile();
+        t.end();
+      });
+    });
+  });
+
+  t.test("fs.lchmod", function (t) {
+    createLink(t);
+
+    var context = namespace.createContext();
+    context.run(function () {
+      namespace.set('test', 'lchmod');
+      t.equal(namespace.get('test'), 'lchmod', "state has been mutated");
+
+      fs.lchmod(LINKNAME, '0700', function (error) {
+        t.notOk(error, "changing mode shouldn't error");
+
+        t.equal(namespace.get('test'), 'lchmod',
+                "mutated state has persisted to fs.lchmod's callback");
+
+        var stats = fs.lstatSync(LINKNAME);
+        t.equal(stats.mode.toString(8), '120700', "extra access bits are stripped");
+
+        deleteLink();
+        t.end();
+      });
+    });
+  });
+
+  t.test("fs.stat", function (t) {
+    createFile(t);
+
+    var context = namespace.createContext();
+    context.run(function () {
+      namespace.set('test', 'stat');
+      t.equal(namespace.get('test'), 'stat', "state has been mutated");
+
+      fs.stat(FILENAME, function (error, stats) {
+        t.notOk(error, "reading stats shouldn't error");
+
+        t.equal(namespace.get('test'), 'stat',
+                "mutated state has persisted to fs.stat's callback");
+
+        t.equal(stats.mode.toString(8), '100666', "permissions should be as created");
+
+        deleteFile();
+        t.end();
+      });
+    });
+  });
+
+  t.test("fs.fstat", function (t) {
+    createFile(t);
+
+    var context = namespace.createContext();
+    context.run(function () {
+      namespace.set('test', 'fstat');
+      t.equal(namespace.get('test'), 'fstat', "state has been mutated");
+
+      var file = fs.openSync(FILENAME, 'r');
+      fs.fstat(file, function (error, stats) {
+        t.notOk(error, "reading stats shouldn't error");
+
+        t.equal(namespace.get('test'), 'fstat',
+                "mutated state has persisted to fs.fstat's callback");
+
+        t.equal(stats.mode.toString(8), '100666', "permissions should be as created");
+
+        fs.closeSync(file);
+        deleteFile();
+        t.end();
+      });
+    });
+  });
+
+  t.test("fs.lstat", function (t) {
+    createLink(t);
+
+    var context = namespace.createContext();
+    context.run(function () {
+      namespace.set('test', 'lstat');
+      t.equal(namespace.get('test'), 'lstat', "state has been mutated");
+
+      fs.lstat(LINKNAME, function (error, stats) {
+        t.notOk(error, "reading stats shouldn't error");
+
+        t.equal(namespace.get('test'), 'lstat',
+                "mutated state has persisted to fs.lstat's callback");
+
+        t.equal(stats.mode.toString(8), '120777', "permissions should be as created");
+
+        deleteLink();
+        t.end();
+      });
+    });
+  });
+
   // TODO: 'link'
+  // TODO: 'unlink'
   // TODO: 'symlink'
   // TODO: 'readlink'
   // TODO: 'realpath'
-  // TODO: 'unlink'
   // TODO: 'rmdir'
   // TODO: 'mkdir'
   // TODO: 'readdir'
